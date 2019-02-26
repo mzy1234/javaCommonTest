8379a346c906079dae34da62900b044113133b0d
mzy1234 <mzy_157@163.com>
2019-02-26 11:56:31 +0800

    222

--- javaBase/src/com/zhiyuan/ma/io/FileDigui.java
+++ javaBase/src/com/zhiyuan/ma/io/FileDigui.java
@@ -1,5 +1,21 @@
 package com.zhiyuan.ma.io;
 
-public class FileDigui {
+import java.io.File;
 
+public class FileDigui {
+    public static void main(String[] args) {
+        File file = new File("D:\\filedemo");
+        getFile(file);
+    }
+    public static void getFile(File file){
+        File[] files = file.listFiles();
+        for (File file1 : files) {
+            if(file1.isFile()){
+                System.out.println("---"+file1.getName());
+            }else if(file1.isDirectory()){
+                System.out.println("---"+file1.getName());
+                getFile(file1);
+            }
+        }
+    }
 }
